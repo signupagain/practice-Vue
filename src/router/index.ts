df28ea3e0aa2fc3297c9dbfe8d476541af5cbf38
jerry <s1062940@yahoo.com>
2024-09-29 01:36:33 +0800

    feat: 頁面滾動行為

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,9 @@ import { routes } from "./routes";
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes,
+	scrollBehavior(to, from, savedPosition) {
+		return savedPosition ? savedPosition : { top: 0 };
+	},
 });
 
 export default router;
